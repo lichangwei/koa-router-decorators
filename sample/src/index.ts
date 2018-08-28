6ea9d23488669d050a014a3108de1c5b83e73eab
louis.li <louis.li@gaiaworks.cn>
2018-08-28 17:59:24 +0800

    修改示例代码

--- sample/src/index.ts
+++ sample/src/index.ts
@@ -2,6 +2,6 @@ import * as Koa from 'koa';
 import * as router from '../../src/index';
 
 const app = new Koa();
-app.use(router.load('/api', `${__dirname}/router`, { extname: '.ts' }).routes());
+app.use(router.load('/api', `${__dirname}/router`, { verbose: true }).routes());
 export default app.listen(9100);
 console.log('Server started: http://localhost:9100');
